dc2130849bfb74471990f43e0bde909f9be4a1f1
Sander Voogt <sandervoogt@outlook.com>
2025-08-27 19:51:27 +0200

    update 3

---
+++ backend/src/admin/carbrands/route.ts
@@ -0,0 +1,48 @@
+import {
+    MedusaRequest,
+    MedusaResponse,
+} from "@medusajs/framework/http"
+import {
+    createBrandWorkflow,
+} from "../../workflows/create-brand"
+import { z } from "zod"
+import { PostAdminCreateCarBrand } from "./validators"
+
+type PostAdminCreateBrandType = z.infer<typeof PostAdminCreateCarBrand>
+
+
+export const POST = async (
+    req: MedusaRequest<PostAdminCreateBrandType>,
+    res: MedusaResponse
+) => {
+    const { result } = await createBrandWorkflow(req.scope)
+        .run({
+            input: req.body,
+        })
+
+    res.json({ a: result })
+}
+
+
+export const GET = async (
+    req: MedusaRequest,
+    res: MedusaResponse
+) => {
+    const query = req.scope.resolve("query")
+
+    const {
+        data: brands,
+        metadata: { count, take, skip } = {},
+    } = await query.graph({
+        entity: "carbrand",
+        fields: ["id", "name", "created_at", "carmodels.*"],
+        ...req.queryConfig,
+    })
+
+    res.json({
+        brands,
+        count,
+        limit: take,
+        offset: skip,
+    })
+}
---
+++ backend/src/admin/middleware.ts
@@ -0,0 +1,81 @@
+import {
+  defineMiddlewares,
+  validateAndTransformBody,
+  validateAndTransformQuery,
+} from "@medusajs/framework/http"
+import { PostAdminCreateCarBrand } from "./../admin/carbrands/validators"
+import { PostAdminCreateCarModel } from "./../admin/carmodels/validators"
+import { z } from "zod"
+import { createFindParams } from "@medusajs/medusa/api/utils/validators"
+import { PostAdminCreateBrand } from "./../admin/brands/validators"
+export const GetBrandsSchema: any = createFindParams()
+
+
+
+export default defineMiddlewares({
+
+
+
+  routes: [
+    {
+      method: "POST",
+      matcher: "/admin/products",
+      additionalDataValidator: {
+        custom_name: z.string().optional(),
+        faq: z.string().optional(),
+        maindescription: z.string().optional()
+      } as any,
+    },
+
+
+    {
+      matcher: "/admin/brands",
+      method: "POST",
+      middlewares: [
+        validateAndTransformBody(PostAdminCreateBrand),
+      ],
+    },
+    {
+      matcher: "/admin/products",
+      method: ["POST"],
+      additionalDataValidator: {
+        brand_id: z.string().optional() as any,
+      },
+
+    },
+    {
+      matcher: "/admin/brands",
+      method: "GET",
+      middlewares: [
+        validateAndTransformQuery(
+          GetBrandsSchema,
+          {
+            defaults: [
+              "id",
+              "name",
+              "products.*",
+            ],
+            isList: true,
+          }
+        ),
+      ],
+    },
+
+
+
+    {
+      matcher: "/admin/carbrands",
+      method: "POST",
+      middlewares: [
+        validateAndTransformBody(PostAdminCreateCarBrand),
+      ],
+    },
+    {
+      matcher: "/admin/carmodels",
+      method: "POST",
+      middlewares: [
+        validateAndTransformBody(PostAdminCreateCarModel),
+      ],
+    },
+  ],
+})
