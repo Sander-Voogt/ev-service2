705df09dcb9b0add1b9cea0e312ad03e15c75c40
Sander Voogt <sandervoogt@outlook.com>
2025-09-07 18:31:07 +0200

    update

---
+++ backend/src/api/store/carbrand/models/route.ts
@@ -0,0 +1,26 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
+
+export const GET = async (
+    req: MedusaRequest,
+    res: MedusaResponse
+) => {
+    const query = req.scope.resolve("query")
+
+    const {
+        data: brands,
+        metadata: { count, take, skip } = {},
+    } = await query.graph({
+        entity: "carbrand",
+        fields: ["name", "id", "created_at", "carmodels.*"],
+        ...req.queryConfig,
+    })
+
+    console.log(brands)
+
+    res.json({
+        brands,
+        count,
+        limit: take,
+        offset: skip,
+    })
+}
