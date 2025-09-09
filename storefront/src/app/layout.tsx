d433b6645425ca28fd3dfd5ff91f0391a190d0f3
Sander Voogt <sandervoogt@outlook.com>
2025-09-09 21:59:13 +0200

    disable google indexing

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <GoogleTagManager gtmId="GTM-KPNGTRPD" />
+      <meta name="robots" content="noindex"></meta>
       <body>
         <main className="relative">{props.children}</main>
       </body>
