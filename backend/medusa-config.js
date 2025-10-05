9177e0359a2918dbf1fda893ef9fb6a56ac7da4c
Sander Voogt <sandervoogt@outlook.com>
2025-10-05 13:39:23 +0200

    stripe payment methods tonen

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -141,6 +141,8 @@ const medusaConfig = {
             options: {
               apiKey: STRIPE_API_KEY,
               webhookSecret: STRIPE_WEBHOOK_SECRET,
+              automatic_payment_methods: true,
+              capture: true
             },
           },
         ],
