4bedaf3c1542db058c135e13ed512526bdc4c97b
Sander Voogt <sandervoogt@outlook.com>
2025-10-08 14:12:56 +0200

    update

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -2,24 +2,24 @@
 
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
-// import MainNavBar from "./components/Mainnavbar"
+import MainNavBar from "./components/Mainnavbar"
 import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import { IconCart } from "@modules/common/components/reusable-nav-elements"
 import { Suspense } from "react"
-import { InfoBar } from "./components/Components"
+// import { InfoBar } from "./components/Components"
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 bg-white shadow-md">
-      <InfoBar currentRegion={regions[0]} />
+      {/* <InfoBar currentRegion={regions[0]} /> */}
       <div className="sticky top-0 inset-x-0 z-50">
         <header className="bg-white">
           <div className="max-w-screen-xl mx-auto flex items-center justify-between py-5 px-4 gap-4 md:gap-8">
-            {/* <MainNavBar /> */}
+            <MainNavBar />
             <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
               <div className="hidden small:flex items-center gap-x-6 h-full">
                 <LocalizedClientLink
