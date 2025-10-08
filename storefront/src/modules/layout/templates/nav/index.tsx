b690d58d4f9a6d0920d4875b8289c52bd66cb12f
Sander Voogt <sandervoogt@outlook.com>
2025-10-08 21:39:30 +0200

    test menu

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,61 +1,84 @@
-import CartButton from "@modules/layout/components/cart-button"
-import SideMenu from "@modules/layout/components/side-menu"
-import MainNavBar from "./components/Mainnavbar"
+import { Suspense } from "react"
+
 import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import {
-  IconCart,
-  IconMail,
-  IconPhone,
-  IconUser,
-  navLink,
-} from "@modules/common/components/reusable-nav-elements"
-import { Suspense } from "react"
-import NavSub from "./components/NavSub"
+import CartButton from "@modules/layout/components/cart-button"
+import SideMenu from "@modules/layout/components/side-menu"
 
 export default async function Nav() {
-  // const regions = await listRegions().then((regions: StoreRegion[]) => regions)
-  // currentRegion={regions[0]} 
+  const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
-    <div className="sticky top-0 inset-x-0 z-50 bg-white shadow-md">
-      <InfoBar /> 
-      <div className="sticky top-0 inset-x-0 z-50">
-        <header className="bg-white">
-          <div className="max-w-screen-xl mx-auto flex items-center justify-between py-5 px-4 gap-4 md:gap-8">
-            <MainNavBar />
-            <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-              <NavSub />
-              <div className="flex items-center gap-2 px-2 py-1 rounded-lg border border-green-200 bg-white hover:shadow transition-shadow">
-                <div className="relative">
-                  <IconCart />
-                </div>
-                <div className="flex flex-col ml-1">
-                  <span className="text-xs font-semibold text-green-900">
-                    Winkelwagen
-                  </span>
-                  {/* <span className="text-xs text-green-default font-bold">
-                    <Suspense
-                      fallback={
-                        <LocalizedClientLink
-                          className="hover:text-ui-fg-base flex gap-2"
-                          href="/cart"
-                          data-testid="nav-cart-link"
-                        >
-                          Winkelwagen (0)
-                        </LocalizedClientLink>
-                      }
-                    >
-                      <CartButton />
-                    </Suspense>
-                  </span> */}
-                </div>
-              </div>
+    <div className="sticky top-0 inset-x-0 z-50 group">
+      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
+        <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+          <div className="flex-1 basis-0 h-full flex items-center">
+            <div className="h-full">
+              <SideMenu regions={regions} />
             </div>
           </div>
-        </header>
-      </div>
+
+          <div className="flex items-center h-full">
+            <LocalizedClientLink
+              href="/"
+              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
+              data-testid="nav-store-link"
+            >
+              Markt-Parasol.nl
+            </LocalizedClientLink>
+          </div>
+
+          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
+            <div className="hidden small:flex items-center gap-x-6 h-full">
+              {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
+                <LocalizedClientLink
+                  className="hover:text-ui-fg-base"
+                  href="/search"
+                  scroll={false}
+                  data-testid="nav-search-link"
+                >
+                  Zoeken
+                </LocalizedClientLink>
+              )}
+              <LocalizedClientLink
+                className="hover:text-ui-fg-base"
+                href="/account"
+                data-testid="nav-account-link"
+              >
+                Account
+              </LocalizedClientLink>
+               <LocalizedClientLink
+                className="hover:text-ui-fg-base"
+                href="/offerte-aanvragen"
+                data-testid="nav-account-link"
+              >
+                Offerte aanvragen
+              </LocalizedClientLink>
+                             <LocalizedClientLink
+                className="hover:text-ui-fg-base"
+                href="/contact"
+                data-testid="nav-account-link"
+              >
+                Contact
+              </LocalizedClientLink>
+            </div>
+            <Suspense
+              fallback={
+                <LocalizedClientLink
+                  className="hover:text-ui-fg-base flex gap-2"
+                  href="/cart"
+                  data-testid="nav-cart-link"
+                >
+                  Winkelwagen (0)
+                </LocalizedClientLink>
+              }
+            >
+              <CartButton />
+            </Suspense>
+          </div>
+        </nav>
+      </header>
     </div>
   )
 }
