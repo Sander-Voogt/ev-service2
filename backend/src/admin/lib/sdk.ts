b74c59d61b50327fd0a6c7cf9e4c3d73bddc4abc
Sander Voogt <sandervoogt@outlook.com>
2025-12-30 14:27:27 +0100

    update

--- backend/src/admin/lib/sdk.ts
+++ backend/src/admin/lib/sdk.ts
@@ -1,9 +1,13 @@
 import Medusa from "@medusajs/js-sdk"
+import { debug } from "util"
 
 export const sdk = new Medusa({
-  baseUrl: process.env.VITE_BACKEND_URL || "/",  // Werkt in Vite (VITE_ prefix) en Node
-  debug: process.env.NODE_ENV === "development",  // Standaard Node/Vite dev check (DEV -> NODE_ENV)
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "/",
+  debug: import.meta.env.DEV,
   auth: {
     type: "session",
   },
 })
+  // baseUrl: process.env.VITE_BACKEND_URL || "/",  // Werkt in Vite (VITE_ prefix) en Node
+  // debug: process.env.NODE_ENV === "development",  // Standaard Node/Vite dev check (DEV -> NODE_ENV)
+
