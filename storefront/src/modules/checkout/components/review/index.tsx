29e376984376ce59a8f0ad0db29cdcf4211c1fae
Sander Voogt <sandervoogt@outlook.com>
2026-02-08 22:34:11 +0100

    sitemap aangepast

--- storefront/src/modules/checkout/components/review/index.tsx
+++ storefront/src/modules/checkout/components/review/index.tsx
@@ -30,7 +30,7 @@ const Review = ({ cart }: { cart: any }) => {
             }
           )}
         >
-          Review
+          Overzicht
         </Heading>
       </div>
       {isOpen && previousStepsCompleted && (
@@ -38,10 +38,19 @@ const Review = ({ cart }: { cart: any }) => {
           <div className="flex items-start gap-x-1 w-full mb-6">
             <div className="w-full">
               <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                By clicking the Place Order button, you confirm that you have
-                read, understand and accept our Terms of Use, Terms of Sale and
-                Returns Policy and acknowledge that you have read Medusa
-                Store&apos;s Privacy Policy.
+                Bij het klikken 'Bestelling plaatsen' ga je akkoord met onze{" "}
+                <a
+                  href="/nl/algemene-voorwaarden"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline"
+                >
+                  Algemene Voorwaarden
+                </a>
+                .
+              </Text>
+              <Text className="txt-small text-ui-fg-muted">
+                Je ontvangt een bevestigingsmail zodra je bestelling is geplaatst.
               </Text>
             </div>
           </div>
