a3d770f467aaec7121b6d716566617b2ba67fcc3
Sander Voogt <sandervoogt@outlook.com>
2026-02-09 08:52:58 +0100

    404 pagina

--- storefront/src/app/[countryCode]/(main)/not-found.tsx
+++ storefront/src/app/[countryCode]/(main)/not-found.tsx
@@ -1,20 +1,24 @@
-import { Metadata } from "next"
-
 import InteractiveLink from "@modules/common/components/interactive-link"
+import Footer from "@modules/layout/templates/footer"
+import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "404",
   description: "Something went wrong",
 }
 
-export default function NotFound() {
+export default async function NotFound() {
   return (
+    <>
+    <Nav />
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">Pagina niet gevonden</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+        De pagina die u probeert te bezoeken bestaat niet.
       </p>
-      <InteractiveLink href="/">Go to frontpage</InteractiveLink>
+      <InteractiveLink href="/">Ga naar de homepagina</InteractiveLink>
     </div>
+    <Footer />
+    </>
   )
 }
--- storefront/src/app/not-found.tsx
+++ storefront/src/app/not-found.tsx
@@ -1,5 +1,7 @@
 import { ArrowUpRightMini } from "@medusajs/icons"
 import { Text } from "@medusajs/ui"
+import Footer from "@modules/layout/templates/footer"
+import Nav from "@modules/layout/templates/nav"
 import { Metadata } from "next"
 import Link from "next/link"
 
@@ -10,21 +12,22 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
-      <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
-      </p>
-      <Link
-        className="flex gap-x-1 items-center group"
-        href="/"
-      >
-        <Text className="text-ui-fg-interactive">Go to frontpage</Text>
-        <ArrowUpRightMini
-          className="group-hover:rotate-45 ease-in-out duration-150"
-          color="var(--fg-interactive)"
-        />
-      </Link>
-    </div>
+    <>
+      <Nav />
+      <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
+        <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+        <p className="text-small-regular text-ui-fg-base">
+          The page you tried to access does not exist.
+        </p>
+        <Link className="flex gap-x-1 items-center group" href="/">
+          <Text className="text-ui-fg-interactive">Go to frontpage</Text>
+          <ArrowUpRightMini
+            className="group-hover:rotate-45 ease-in-out duration-150"
+            color="var(--fg-interactive)"
+          />
+        </Link>
+      </div>
+      <Footer />
+    </>
   )
 }
