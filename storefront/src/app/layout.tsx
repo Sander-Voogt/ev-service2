20f7f81aae92b9520e87c28a82b245691bffa4f4
Sander Voogt <sandervoogt@outlook.com>
2026-02-10 22:53:51 +0100

    fix

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
       <link rel="alternate" href={`${process.env.NEXT_PUBLIC_BASE_URL}/nl/`} hrefLang="nl-nl" />
       <link rel="alternate" href={`${process.env.NEXT_PUBLIC_BASE_URL}/be/`} hrefLang="nl-be" />
       <link rel="alternate" href={`${process.env.NEXT_PUBLIC_BASE_URL}/nl/`} hrefLang="x-default" />
-      <meta name="robots" content="noindex"></meta>
+      <meta name="robots" content="index"></meta>
       <body>
         <main className="relative">{props.children}</main>
       </body>
