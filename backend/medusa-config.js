9046d855490224386d85e9dc44d84db359077613
Sander Voogt <Sander-Voogt@users.noreply.github.com>
2026-02-18 12:22:58 +0100

    Disable automatic payment methods in config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -182,7 +182,8 @@ const medusaConfig = {
                   options: {
                     apiKey: STRIPE_API_KEY,
                     webhookSecret: STRIPE_WEBHOOK_SECRET,
-                    automatic_payment_methods: true,
+                    automatic_payment_methods: false,
+                    payment_method_types: ["card", "ideal", "bancontact"],
                     capture: true,
                   },
                 },
